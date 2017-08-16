af01b28b6bcdd1379078c3bde1487deb82f6675e
JYovan <metalrock3@hotmail.es>
2017-08-16 02:11:01 -0500

    NO SIRVE ESTE CODIGO DE GET RECORDS...

--- application/views/vGeneral.php
+++ application/views/vGeneral.php
@@ -45,48 +45,6 @@
         });
         
     });
-
-    function getRecords() {
-        HoldOn.open({theme: "sk-bounce", message: "CARGANDO DATOS..."});
-        $.ajax({
-            url: master_url + 'getRecords',
-            type: "POST",
-            dataType: "JSON"
-        }).done(function (data, x, jq) {
-            $("#tblRegistros").html(getTable('tblTrabajos', data));
-            $('#tblTrabajos tfoot th').each(function () {
-                var title = $(this).text();
-                $(this).html('<div class="col-md-12" style="overflow-x:auto; "><div class="form-group Customform-group"><input type="text" placeholder="Buscar por ' + title + '" class="form-control" style="width: 100%;"/></div></div>');
-            });
-            var tblSelected = $('#tblTrabajos').DataTable(tableOptions);
-            tblSelected.columns().every(function () {
-                var that = this;
-                $('input', this.footer()).on('keyup change', function () {
-                    if (that.search() !== this.value) {
-                        that.search(this.value).draw();
-                    }
-                });
-            });
-            $('#tblTrabajos tbody').on('click', 'tr', function () {
-                $("#tblTrabajos").find("tr").removeClass("success");
-                $("#tblTrabajos").find("tr").removeClass("warning");
-                var id = this.id;
-                var index = $.inArray(id, selected);
-                if (index === -1) {
-                    selected.push(id);
-                } else {
-                    selected.splice(index, 1);
-                }
-                $(this).addClass('success');
-                var dtm = tblSelected.row(this).data();
-
-
-            });
-        }).fail(function (x, y, z) {
-            console.log(x, y, z);
-        }).always(function () {
-            HoldOn.close();
-        });
-    }
+ 
 
 </script>
